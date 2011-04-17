609fa2b28cf1a2b43dae6bcd7402f7f8d0903a20
barry.cswords@gmail.com <barry.cswords@gmail.com@50e1fe55-4f61-e49c-31a2-1de2d0a10630>
2011-04-17 11:22:37 +0000

    shit

--- SearchClient-GUI/WebContent/script/search.js
+++ SearchClient-GUI/WebContent/script/search.js
@@ -3,19 +3,12 @@
  */
 function query() {
 	var query = document.getElementById("searchText").value;
+	query = encodeURIComponent(query);
 	var psize = document.getElementById("pageSize").value;
 	var pnum = document.getElementById("pageNumber").value;
-	
-	// set the type of encoding to numerical entities e.g & instead of &
-	Encoder.EncodeType = "numerical";
+	var from = document.getElementById("fromDate").value;
+	var to = document.getElementById("toDate").value;
 
-	// or to set it to encode to html entities e.g & instead of &
-	Encoder.EncodeType = "entity";
-
-	// HTML encode text from an input element
-	// This will prevent double encoding.
-	query = encodeURIComponent(query);
-	
 	if (document.getElementById("dogoogle").checked) {
 		var qgoogleweb = './service?qtype=googleweb&page=' + pnum + '&size='
 				+ psize + '&q=' + query;
@@ -40,6 +33,10 @@ function query() {
 	if (document.getElementById("doflickr").checked) {
 		var qflickr = './service?qtype=flickr&page=' + pnum + '&size=' + psize
 				+ '&q=' + query;
+		if (from.length > 0)
+			qflickr += '&from=' + encodeURIComponent(from);
+		if (to.length > 0)
+			qflickr += '&to=' + encodeURIComponent(to);
 		$('#flickr').load(qflickr);
 	} else {
 		$('#flickr').html('');
@@ -47,6 +44,10 @@ function query() {
 	if (document.getElementById("dopicasa").checked) {
 		var qpicasa = './service?qtype=picasa&page=' + pnum + '&size=' + psize
 				+ '&q=' + query;
+		if (from.length > 0)
+			qpicasa += '&from=' + encodeURIComponent(from);
+		if (to.length > 0)
+			qpicasa += '&to=' + encodeURIComponent(to);
 		$('#picasa').load(qpicasa);
 	} else {
 		$('#picasa').html('');
@@ -70,3 +71,15 @@ function lastPage() {
 		query();
 	}
 }
+
+var pickerOpts = {
+ dateFormat: "dd/mm/yy"
+};
+
+$(document).ready(function() {
+	$("#fromDate").datepicker(pickerOpts);
+});
+
+$(document).ready(function() {
+	$("#toDate").datepicker(pickerOpts);
+});
